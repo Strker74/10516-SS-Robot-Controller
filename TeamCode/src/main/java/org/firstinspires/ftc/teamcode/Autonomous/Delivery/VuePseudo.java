6dbeb30c9fe19c67b37631c7197c290550ea589f
Akhil Ganesan <strker74@gmail.com>
2019-12-18 13:08:04 -0500

    EN Pseudocode Vuforia Trackables Upload 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Delivery/VuePseudo.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Delivery;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+@Autonomous(name = "EN Pseudo Code [Don't Run]", group = "Pseudo Code")
+@Disabled
+public class VuePseudo {
+    private void grab() {
+        // This is a method which is supposed to
+        // program the robot's grabbing mechanism.
+        // Mostly just a series of commands
+    }
+    private void search(String target) {
+        // This method iterates through
+    }
+}
