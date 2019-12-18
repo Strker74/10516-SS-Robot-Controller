7e5952fe242b9aeb2ac01167e96c46d709b2e3f6
Akhil Ganesan <strker74@gmail.com>
2019-12-18 16:45:17 -0500

    Vue 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Delivery/VuePseudo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Delivery/VuePseudo.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Autonomous.Delivery;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.vuforia.Vuforia;
 
 @Autonomous(name = "EN Pseudo Code [Don't Run]", group = "Pseudo Code")
 @Disabled
@@ -12,6 +13,15 @@ public class VuePseudo {
         // Mostly just a series of commands
     }
     private void search(String target) {
-        // This method iterates through
+        int n = Vuforia.Trackables.find(target);
+        if (Vuforia.Trackables.isVisible(n) != true) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    @Override
+    public void runOpMode() {
+
     }
 }
